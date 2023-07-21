40392aa746bb35bc2f6fd53d3756ca22a1fe6dc8
Simon K <30438951+simshard@users.noreply.github.com>
2023-07-21 23:58:28 +0100

    list chirps

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -14,6 +14,15 @@
         <InputError :message="form.errors.message" class="mt-2" />
         <PrimaryButton class="mt-4">Chirp</PrimaryButton>
    </form>
+   <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+
+<Chirp
+    v-for="chirp in chirps"
+    :key="chirp.id"
+    :chirp="chirp"
+/>
+
+</div>
  </div>
  </AuthenticatedLayout>
 </template>
@@ -22,7 +31,9 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Chirp from '@/Components/Chirp.vue';
 import { useForm, Head } from '@inertiajs/vue3';
+defineProps(['chirps']);
 const form = useForm({
     message: '',
 });
